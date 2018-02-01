eb6a82ed9ff5d96d8c1c38220cc098094284fc24
Daniel Klein <daniel@kleintime.ca>
2018-01-31 23:26:57 -0800

    adding in echo messages

--- comp/8006/assignment2/scripts/install.sh
+++ comp/8006/assignment2/scripts/install.sh
@@ -2,31 +2,38 @@
 source ./config.sh
 
 function gateway_config {
+	echo "Configuring gateway ($gatewayPubHost/$intGatewayHostId)..."
 	$IFG $gatewaySecondaryNetCard $intGatewayHostId up
 	echo "1" >/proc/sys/net/ipv4/ip_forward
 	route add -net $intNetworkAdd netmask $gatewayNetmask gw $gatewayPubHost
 	route add -net $intNetworkAdd/24 gw $intGatewayHostId
+	echo "Gateway configuration complete!"
 }
 
 function restore_all {
 	## Clear iptables settings ##
+	echo "Clearing old settings..."
 	$IPT -P INPUT ACCEPT
 	$IPT -P OUTPUT ACCEPT
 	$IPT -P FORWARD ACCEPT
 	$IPT -Z
 	$IPT -F
 	$IPT -X
+	echo "Deleting routes..."
 	route del default gw $intGatewayHostId
 	route del -net $intNetworkAdd netmask $gatewayNetmask gw $gatewayPubHost
 	route del -net $intNetworkAdd/24 gw $intGatewayHostId
+	echo "Reset complete!"
 }
 
 function setup_default {
+	echo "Setting default to DROP..."
 	## Set default policies to DROP ##
 	$IPT -P INPUT DROP
 	$IPT -P OUTPUT DROP
 	$IPT -P FORWARD DROP
 	## Set up masquerade for outgoing ##
+	echo "Adding FORWARD rules for internal network..."
 	$IPT -t nat -A POSTROUTING -o $gatewayPrimaryNetCard -j MASQUERADE
 	## Set up forwarding to internal network ##
 	$IPT -A FORWARD -i $gatewayPrimaryNetCard -o $gatewaySecondaryNetCard -m state --state NEW,ESTABLISHED -j ACCEPT
@@ -38,6 +45,7 @@ function setup_default {
 
 function custom_chains {
 	## Custom chains ##
+	echo "Creating custom chains..."
 	$IPT -N TCP_CHAIN
 	$IPT -N UDP_CHAIN
 	$IPT -N INVALID_CHAIN
@@ -56,6 +64,7 @@ function custom_chains {
 	## Allow TCP ##
 	for tcp_port in ${tcp_ports[@]}
 	do
+		echo "Allowing TCP on port $tcp_port..."
 		$IPT -A TCP_CHAIN -m tcp -p tcp --dport $tcp_port -m conntrack --ctstate  NEW,ESTABLISHED -j ACCEPT
 		$IPT -t nat -A PREROUTING -p tcp -i $gatewayPrimaryNetCard --dport $tcp_port -j DNAT --to $intClientHostId:$tcp_port
 		#$IPT -t nat -A PREROUTING -i $gatewayPrimaryNetCard  -p tcp --dport $tcp_port -j REDIRECT --to-destination $intClientHostId:$tcp_port
@@ -63,12 +72,13 @@ function custom_chains {
 	## Allow UDP ##
 	for udp_port in ${udp_ports[@]}
 	do
+		echo "Allowing UDP on port $udp_port"
 		$IPT -A UDP_CHAIN -m udp -p udp --dport $udp_port -j ACCEPT
 	done
 	## ICMP ##
 	for icmp_type in ${icmp_types[@]}
 	do
-		echo "allowing icmp type $icmp_type"
+		echo "Allowing icmp type $icmp_type..."
 		$IPT -A ICMP_CHAIN -p icmp -s $intNetworkAdd/24 --icmp-type $icmp_type -j ACCEPT
 		$IPT -A ICMP_CHAIN -p icmp -s $gatewayNetwork/24 --icmp-type $icmp_type -j ACCEPT
 		$IPT -t nat -A PREROUTING -p icmp -s $gatewayNetwork/24 --icmp-type $icmp_type -j DNAT --to $intClientHostId
@@ -87,6 +97,7 @@ function custom_chains {
 }
 
 function set_tos {
+	echo "Setting TOS..."
 	## SSH ##
 	$IPT -A PREROUTING -t mangle -p tcp --sport 22 -j TOS --set-tos Minimize-Delay
 	$IPT -A OUTPUT -t mangle -p tcp --dport 22 -j TOS --set-tos Minimize-Delay
@@ -99,6 +110,7 @@ function set_tos {
 }
 
 function internal_config {
+	echo "Configuring internal host ($intClientHostId)..."
 	$IFG $intPrimaryNetCard down
 	$IFG $intSecondaryNetCard $intClientHostId
 	route add default gw $intGatewayHostId
